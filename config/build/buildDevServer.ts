adf3f042c663b9aeb34ecf62ebdfd9fdc5dad27a
Azizbek Aktamov <aktamovazizbek563@gmail.com>
2026-06-11 16:15:05 +0500

    first com

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -5,7 +5,7 @@ import type {Configuration as DevServerConfiguration} from "webpack-dev-server"
 export function buildDevServer(options:BuildOptions): DevServerConfiguration {
         return {
             port: options.port,
-            open: options.isDev
+            open: options.isDev,
             historyApiFallback: true,
             hot: true,
         }
